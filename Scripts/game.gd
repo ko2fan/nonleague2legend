be9546522da352503c8b93200b4d4ede05d1b6b3
David Athay <ko2fan@gmail.com>
2024-09-14 11:51:20 -0500

    Defer the call to loading the menu at start time

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -3,4 +3,4 @@ extends Node2D
 @onready var menu = preload("res://Scenes/menu.tscn")
 
 func _ready():
-	get_tree().change_scene_to_packed(menu)
+	get_tree().call_deferred("change_scene_to_packed", menu)
