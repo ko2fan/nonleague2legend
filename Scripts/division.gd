2bf7dc882348dfe7b19e5221628e33a384784974
David Athay <ko2fan@gmail.com>
2024-09-22 01:23:37 -0500

    Fix fixture bug

--- Scripts/division.gd
+++ Scripts/division.gd
@@ -17,6 +17,9 @@ func create_season_fixtures():
 		var weekly_fixtures = []
 		for x in range(0, N / 2):
 			weekly_fixtures.append({ "home_team": (x + i) % N, "away_team": (N-1 - x + i) % N })
+		fixtures.append(weekly_fixtures)
+	for i in range(0, N - 1):
+		var weekly_fixtures = []
 		for x in range((N / 2) + 1, N):
 			weekly_fixtures.append({ "home_team": (x + i) % N, "away_team": (N-1 - x + i) % N })
 		fixtures.append(weekly_fixtures)
